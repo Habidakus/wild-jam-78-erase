42278cbfa12d264e68f84d715debd3e0dc560f8d
Jason Abbott <woebane@gmail.com>
2025-02-23 11:32:05 -0800

    Added credits page

--- Scripts/menu.gd
+++ Scripts/menu.gd
@@ -2,3 +2,6 @@ extends StateMachineState
 
 func _on_button_button_up() -> void:
 	our_state_machine.switch_state("Game")
+
+func _on_credits_button_up() -> void:
+	our_state_machine.switch_state("Credits")
